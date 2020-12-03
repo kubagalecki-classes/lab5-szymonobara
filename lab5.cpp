292b7a616e9f34388ebc355f093d901557e8d607
szymonobara <Szymon2904@gmail.com>
2020-12-03 12:51:36 +0000

    a

--- lab5.cpp
+++ lab5.cpp
@@ -1,6 +1,22 @@
+#include "Human.hpp"
 #include <iostream>
-
+#include <vector>
 int main()
 {
-    puts("Hello, World!");
+    std::vector< Human > v;
+    std::string          com;
+    std::string          name;
+    for (int i = 0; i < 5; i++) {
+        std::cin >> name;
+        Human h{name, 15, true, true};
+        v.push_back(h);
+    }
+    std::cout << v.size() << ", " << v[0].getName() << "\n";
+    std::cin >> com;
+
+    if (com == "pop")
+        v.pop_back();
+    for (int i = 0; i < v.size(); i++) {
+        std::cout << v[i].getName() << "\n";
+    }
 }

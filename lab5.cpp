dcb2a9538b6204bad79d7e048222ddafaeefde8c
szymonobara <Szymon2904@gmail.com>
2020-12-04 12:29:16 +0000

    x

--- lab5.cpp
+++ lab5.cpp
@@ -1,22 +1,35 @@
 #include "Human.hpp"
 #include <iostream>
+#include <list>
 #include <vector>
 int main()
 {
-    std::vector< Human > v;
-    std::string          com;
-    std::string          name;
-    for (int i = 0; i < 5; i++) {
-        std::cin >> name;
-        Human h{name, 15, true, true};
-        v.push_back(h);
-    }
-    std::cout << v.size() << ", " << v[0].getName() << "\n";
-    std::cin >> com;
+    // std::vector< Human > v;
 
-    if (com == "pop")
-        v.pop_back();
-    for (int i = 0; i < v.size(); i++) {
-        std::cout << v[i].getName() << "\n";
-    }
+    // std::string com;
+    // std::string name;
+    // for (int i = 0; i < 5; i++) {
+    //     std::cin >> name;
+    //     Human h{name, 15, true, true};
+    //     v.push_back(h);
+    // }
+    // std::vector< Human >::iterator it;
+    // std::cout << v.size() << ", " << v[0].getName() << "\n";
+    // std::cin >> com;
+
+    // if (com == "pop")
+    //     v.pop_back();
+    // for (it = v.begin(); it != v.end(); it++) {
+    //     std::cout << it->getName() << "\n";
+    // }
+    std::list< int > list;
+    list.push_back(1);
+    std::cout << list.front() << ", " << list.back() << "\n";
+    list.push_front(2);
+    std::cout << list.front() << ", " << list.back() << "\n";
+    std::cout << &list.front() << ", " << &list.back() << "\n";
+    list.push_front(5);
+    list.push_front(3);
+    list.push_front(4);
+    std::list< int >::const_reverse_iterator it = list.rbegin();
 }
--- zal/foo.hpp
+++ zal/foo.hpp
@@ -7,6 +7,21 @@
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    // Twoja implementacja tutaj
-    return {};
+    char x='n';
+    char y='y';
+    std::list<Human>::iterator it = people.begin();
+    
+    
+    for(; it != people.end(); it++)
+      it->birthday();
+    std::list<Human>::const_reverse_iterator rit = people.rbegin();
+    std::vector<char> v;
+    for(; it!=people.rend(); it++)
+    {
+      if(it->isMonster() == true)
+        v.push_back(x);
+      else
+        v.push_back(y);
+    }
+    return v;
 }
